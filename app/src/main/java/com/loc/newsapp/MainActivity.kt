809d4f06d25709397af84f59f0d64fd26bbf0304
Vishu <champvikash4@gmail.com>
2023-11-25 01:20:39 -0800

    NewsApp_UI

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -10,14 +10,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
 import com.loc.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             NewsAppTheme {
-
+                OnBoardingScreen()
             }
         }
     }
---
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/Dimense.kt
@@ -0,0 +1,10 @@
+package com.loc.newsapp.presentation.onboarding
+
+import androidx.compose.ui.unit.dp
+
+object Dimense {
+    val hightBtw1  = 24.dp
+    val hightBtw2  = 30.dp
+    val IndicatSize = 14.dp
+
+}
